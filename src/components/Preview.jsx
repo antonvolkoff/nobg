c3c1ac278733656543ad02d21e6cc75292130359
Anton Volkov <choixer@gmail.com>
2024-03-19 15:36:25 +0100

    Default state for preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,18 +1,26 @@
 export default function Preview({ original, result }) {
-  return (
-    <div className="preview">
-      <div>
-        <div className="preview-title">Original</div>
-        <div className="preview-item">
-          <img src={original} />
+  if (original) {
+    return (
+      <div className="preview">
+        <div>
+          <div className="preview-title">Original</div>
+          <div className="preview-item">
+            <img src={original} />
+          </div>
+        </div>
+        <div>
+          <div className="preview-title">Processed</div>
+          <div className="preview-item">
+            <img src={result} />
+          </div>
         </div>
       </div>
-      <div>
-        <div className="preview-title">Processed</div>
-        <div className="preview-item">
-          <img src={result} />
-        </div>
+    );
+  } else {
+    return (
+      <div className="preview">
+        Click "New Image" to start
       </div>
-    </div>
-  );
+    );
+  }
 }
